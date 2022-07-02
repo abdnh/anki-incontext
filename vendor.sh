7305d0b8e8eed16ada4329b240d255e6320d2312
Abdo <abd.nh25@gmail.com>
2022-07-03 01:33:09 +0300

    Trim vendored pycountry

--- vendor.sh
+++ vendor.sh
@@ -3,3 +3,4 @@
 curl https://raw.githubusercontent.com/abdnh/tdk/master/tdk.py -o src/providers/vendor/tdk.py
 curl https://raw.githubusercontent.com/abdnh/skell-downloader/master/skell_downloader.py -o src/providers/vendor/skell_downloader.py
 cp -r submodules/pycountry/src/pycountry src/providers/vendor/.
+rm -rf src/providers/vendor/pycountry/locales src/providers/vendor/pycountry/tests
