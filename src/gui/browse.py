ae64026fa1cebc206de19877b8023a661f719769
Abdo <abdo@abdnh.net>
2025-10-30 15:27:09 +0300

    Fix initial search language not set correctly

--- src/gui/browse.py
+++ src/gui/browse.py
@@ -15,15 +15,19 @@ class BrowseDialog(SveltekitWebDialog):
         parent: QWidget | None = None,
         word: str = "",
         language: str = "",
-        providers: list[str] = [],
+        providers: list[str] | None = None,
         auto_search: bool = False,
     ):
-        self._kwargs = {
-            "word": word,
-            "language": language,
-            "providers": ",".join(providers),
+        self._kwargs: dict[str, Any] = {
             "auto": auto_search,
         }
+        if word:
+            self._kwargs["word"] = word
+        if language:
+            self._kwargs["language"] = language
+        if providers is not None:
+            self._kwargs["providers"] = providers
+
         super().__init__(path="browse", parent=parent, subtitle="Browse Sentences")
 
     def get_query_params(self) -> dict[str, Any]:
