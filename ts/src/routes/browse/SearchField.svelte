5b17797a13ea5f9853d1691438de7fe06878a629
Abdo <abdo@abdnh.net>
2026-01-04 23:35:48 +0300

    Add a shortcut to focus search field

--- ts/src/routes/browse/SearchField.svelte
+++ ts/src/routes/browse/SearchField.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
+    let inputElement: HTMLInputElement;
+
     let { onSearch, value = $bindable(""), autoTrigger = false }: {
         onSearch: () => void;
         value: string;
@@ -11,6 +13,9 @@
         if (event.key === "Enter") {
             onSearch();
         }
+        if (event.code === "KeyF" && event.ctrlKey) {
+            inputElement.focus();
+        }
     }
 
     onMount(() => {
@@ -20,13 +25,15 @@
     });
 </script>
 
+<svelte:document onkeydown={onKeyDown}></svelte:document>
+
 <div class="search-field">
     <input
         type="text"
         class="form-control"
         placeholder="Type a word to search..."
+        bind:this={inputElement}
         bind:value={value}
-        onkeydown={onKeyDown}
     />
     <button class="btn btn-primary" onclick={onSearch} aria-label="Search">
         <i class="bi bi-search"></i>
